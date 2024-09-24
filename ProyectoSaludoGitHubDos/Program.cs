4e7b56264fa939dfbd493f0beff3e04a654535de
MaxYbrNotebook <MaxYbrNotebook@DESKTOP-83HACU0>
2024-09-23 23:35:34 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoSaludoGitHubDos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSaludoGitHubDos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Segundo saludo GitHub!!");
+            Console.ReadKey();
+        }
+    }
+}
